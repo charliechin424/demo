1bce2db480e2802ce225b6f4e2f606d674c0cd61
charlie <charlie920424@gmail.com>
2023-04-05 02:04:17 +0800

    Lab05

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <h1>Welcome!</h1>
       <ul style={{width: "100%"}}>
         <li style={{padding: "1%"}}>Name : 金家逸</li>
-        <li style={{padding: "1%"}}>Student ID : B10502076</li>
+        <li style={{padding: "1%"}}>Student ID : B10502076 </li>
         <li style={{padding: "1%"}}>Department : Sophomore of Electrical Engineering</li>
         <li style={{padding: "1%"}}>Genger : male</li>
       </ul>
